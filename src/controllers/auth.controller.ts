45838deca93ad88817be3a1e486b24b9f6397b38
Safa-Anan08 <safaspractice@gmail.com>
2026-07-16 00:14:09 +0600

    google login

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -137,7 +137,7 @@ export const googleLogin = async (
       token,
       {
         httpOnly:true,
-        secure:false,
+        secure:process.env.NODE_ENV === "production",
         sameSite:"lax",
         maxAge:
           7*24*60*60*1000
